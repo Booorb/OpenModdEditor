53df4bdf6c6a69da15cf51d7bef09f13900019cd
Borb <70481781+Booorb@users.noreply.github.com>
2023-07-16 11:24:55 +0200

    better code formatting

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,12 @@
 import dearpygui.dearpygui as dpg
 import json
 
+
 def back_callback():
     if dpg.does_item_exist("game_settings"):
         dpg.show_item("default_window")
         dpg.set_primary_window("default_window", True)
+
 
 def edit_callback():
     if dpg.does_item_exist("game_settings"):
@@ -24,48 +26,84 @@ def edit_callback():
                         dpg.add_input_text(tag="game_slug")
                         dpg.set_value(value=data["gameSlug"], item="game_slug")
                         dpg.add_text("Repository Access:")
-                        dpg.add_listbox(items=['Open Source', 'Private'], tag="repository_access")
+                        dpg.add_listbox(
+                            items=["Open Source", "Private"], tag="repository_access"
+                        )
                         if data["access"] == "private":
                             dpg.set_value(value="Private", item="repository_access")
                         else:
                             dpg.set_value(value="Open Source", item="repository_access")
                         dpg.add_text("Enable video chat:")
-                        dpg.add_checkbox(default_value=data["enableVideoChat"], tag="enable_video_chat")
+                        dpg.add_checkbox(
+                            default_value=data["enableVideoChat"],
+                            tag="enable_video_chat",
+                        )
                         dpg.add_text("Hidden:")
                         dpg.add_checkbox(default_value=data["hidden"], tag="hidden")
                         dpg.add_text("Enable Data Saving:")
-                        dpg.add_checkbox(default_value=data["enablePersistedData"], tag="enable_persistent_data")
+                        dpg.add_checkbox(
+                            default_value=data["enablePersistedData"],
+                            tag="enable_persistent_data",
+                        )
                         dpg.add_text("Enable Context Menu:")
-                        dpg.add_checkbox(default_value=data["contextMenuEnabled"], tag="context_menu")
+                        dpg.add_checkbox(
+                            default_value=data["contextMenuEnabled"], tag="context_menu"
+                        )
                         dpg.add_text("Daily coin transfer limit:")
-                        dpg.add_slider_int(default_value=data["dailyCoinTransferLimit"], tag="transfer_limit")
+                        dpg.add_slider_int(
+                            default_value=data["dailyCoinTransferLimit"],
+                            tag="transfer_limit",
+                        )
                         dpg.add_button(label="Save", callback=save_callback)
                 with dpg.menu(label="Editor"):
                     dpg.add_menu_item(label="Theme")
-                    dpg.add_menu_item(label="Toggle Fullscreen", callback=fullscreen_callback)
+                    dpg.add_menu_item(
+                        label="Toggle Fullscreen", callback=fullscreen_callback
+                    )
                     dpg.add_menu_item(label="Update", callback=fullscreen_callback)
                 with dpg.menu(label="Engine"):
                     dpg.add_text("Maximum Players:")
-                    dpg.add_slider_int(default_value=data["defaultMaxPlayers"], tag="max_players")
+                    dpg.add_slider_int(
+                        default_value=data["defaultMaxPlayers"], tag="max_players"
+                    )
                     dpg.add_text("Server Life Span:")
-                    dpg.add_slider_int(default_value=data["lifeSpanHours"], max_value=6, tag="server_life_span")
+                    dpg.add_slider_int(
+                        default_value=data["lifeSpanHours"],
+                        max_value=6,
+                        tag="server_life_span",
+                    )
                     dpg.add_text("Physics Engine:")
-                    dpg.add_listbox(items=['PlanckJS', 'Box2dWeb', 'Box2d es6'], tag="physics_engine")
+                    dpg.add_listbox(
+                        items=["PlanckJS", "Box2dWeb", "Box2d es6"],
+                        tag="physics_engine",
+                    )
                     dpg.add_text("Client Physics Engine:")
-                    dpg.add_listbox(items=['PlanckJS', 'Box2dWeb', 'Box2d es6'], tag="client_physics_engine")
+                    dpg.add_listbox(
+                        items=["PlanckJS", "Box2dWeb", "Box2d es6"],
+                        tag="client_physics_engine",
+                    )
                     dpg.add_text("Rendering Filter:")
-                    dpg.add_listbox(items=['Smooth', 'Pixel Art'], tag="rendering_filter")
+                    dpg.add_listbox(
+                        items=["Smooth", "Pixel Art"], tag="rendering_filter"
+                    )
                     dpg.add_text("Client-side predicted movement:")
-                    dpg.add_checkbox(default_value=data["clientSidePredictionEnabled"], tag="predicted_movement")
+                    dpg.add_checkbox(
+                        default_value=data["clientSidePredictionEnabled"],
+                        tag="predicted_movement",
+                    )
                     dpg.add_text("Physics frame rate:")
-                    dpg.add_slider_int(default_value=15, max_value=60, tag="physics_frame_rate")
+                    dpg.add_slider_int(
+                        default_value=15, max_value=60, tag="physics_frame_rate"
+                    )
                     dpg.add_text("Gravity:")
                     dpg.add_slider_int(label="x", default_value=0, tag="gravity_x")
                     dpg.add_slider_int(label="y", default_value=0, tag="gravity_y")
                     dpg.add_text("Allow duplicate IP's:")
-                    dpg.add_checkbox(default_value=data["allowDuplicateIPS"], tag="duplicate_ips")
+                    dpg.add_checkbox(
+                        default_value=data["allowDuplicateIPS"], tag="duplicate_ips"
+                    )
                     dpg.add_button(label="Save", callback=save_callback)
-                    
+
                 with dpg.menu(label="UI"):
                     print("UI")
                 with dpg.menu(label="Map"):
@@ -101,7 +139,8 @@ def save_callback(sender):
         data["allowDuplicateIPS"] = dpg.get_value("duplicate_ips")
         data["data"]["settings"]["gravity"]["x"] = dpg.get_value("gravity_x")
         data["data"]["settings"]["gravity"]["y"] = dpg.get_value("gravity_y")
-        json.dump(data, open("taro2/src/game.json", "w"), indent = 4)
+        json.dump(data, open("taro2/src/game.json", "w"), indent=4)
+
 
 def fullscreen_callback():
     dpg.toggle_viewport_fullscreen()
--- src/setup.py
+++ src/setup.py
@@ -6,9 +6,11 @@ import shutil
 import json
 from settings import edit_callback
 
+
 def play_callback():
     os.system("cd taro2 && npm run server&")
-    webbrowser.open('http://localhost:3000/', new=2)
+    webbrowser.open("http://localhost:3000/", new=2)
+
 
 def setup_project_callback():
     if dpg.does_item_exist("setup_project"):
@@ -19,36 +21,86 @@ def setup_project_callback():
                 dpg.add_text("Taro2 is already installed!")
             else:
                 dpg.add_text("Download Taro2:", tag="taro2_text")
-                dpg.add_button(label="Download", tag="taro2_button", callback=taro2_callback)
+                dpg.add_button(
+                    label="Download", tag="taro2_button", callback=taro2_callback
+                )
 
             if os.path.isdir("taro2/node_modules"):
                 dpg.add_text("Already installed npm packages!")
             else:
                 dpg.add_text("Download npm packages:", tag="packages_text")
-                dpg.add_button(label="Download", tag="packages_button", callback=packages_callback)
+                dpg.add_button(
+                    label="Download", tag="packages_button", callback=packages_callback
+                )
             if os.path.isfile("taro2/src/game.json"):
                 dpg.add_text("Already added game.json!")
             else:
                 dpg.add_text("Download template game or upload own game.json file:")
-                dpg.add_button(label="Battle Royale", tag="battle_royale_button", callback=battle_royale_callback)
-                dpg.add_button(label="Blank Template", tag="blank_template_button", callback=blank_template_callback)
-                dpg.add_button(label="Cell Eater", tag="cell_eater_button", callback=cell_eater_callback)
-                dpg.add_button(label="Deathmatch", tag="deathmatch_button", callback=deathmatch_callback)
-                dpg.add_button(label="Guided Tutorial", tag="guided_tutorial_button", callback=guided_tutorial_callback)
-                dpg.add_button(label="Hunt and Gather", tag="hunt_and_gather_button", callback=hunt_and_gather_callback)
-                dpg.add_button(label="Platformer", tag="platformer_button", callback=platformer_callback)
-                dpg.add_button(label="Racer", tag="racer_button", callback=racer_callback)
-                dpg.add_button(label="Soccer", tag="soccer_button", callback=soccer_callback)
-                dpg.add_button(label="Team Elimination", tag="team_elimination_button", callback=team_elimination_callback)
-                dpg.add_button(label="Tower Defense", tag="tower_defense_button", callback=tower_defense_callback)
-                dpg.add_button(label="Zombie Tag", tag="zombie_tag_button", callback=zombie_tag_callback)
+                dpg.add_button(
+                    label="Battle Royale",
+                    tag="battle_royale_button",
+                    callback=battle_royale_callback,
+                )
+                dpg.add_button(
+                    label="Blank Template",
+                    tag="blank_template_button",
+                    callback=blank_template_callback,
+                )
+                dpg.add_button(
+                    label="Cell Eater",
+                    tag="cell_eater_button",
+                    callback=cell_eater_callback,
+                )
+                dpg.add_button(
+                    label="Deathmatch",
+                    tag="deathmatch_button",
+                    callback=deathmatch_callback,
+                )
+                dpg.add_button(
+                    label="Guided Tutorial",
+                    tag="guided_tutorial_button",
+                    callback=guided_tutorial_callback,
+                )
+                dpg.add_button(
+                    label="Hunt and Gather",
+                    tag="hunt_and_gather_button",
+                    callback=hunt_and_gather_callback,
+                )
+                dpg.add_button(
+                    label="Platformer",
+                    tag="platformer_button",
+                    callback=platformer_callback,
+                )
+                dpg.add_button(
+                    label="Racer", tag="racer_button", callback=racer_callback
+                )
+                dpg.add_button(
+                    label="Soccer", tag="soccer_button", callback=soccer_callback
+                )
+                dpg.add_button(
+                    label="Team Elimination",
+                    tag="team_elimination_button",
+                    callback=team_elimination_callback,
+                )
+                dpg.add_button(
+                    label="Tower Defense",
+                    tag="tower_defense_button",
+                    callback=tower_defense_callback,
+                )
+                dpg.add_button(
+                    label="Zombie Tag",
+                    tag="zombie_tag_button",
+                    callback=zombie_tag_callback,
+                )
+
 
 def taro2_callback():
     dpg.delete_item("taro2_button")
     dpg.set_value(value="Started downloading taro2...", item="taro2_text")
     os.system("git clone https://github.com/moddio/taro2.git")
-    filereplace("taro2/server/server.js","80","3000")
+    filereplace("taro2/server/server.js", "80", "3000")
     dpg.set_value(value="Finished downloading taro2...", item="taro2_text")
+
 
 def packages_callback():
     dpg.delete_item("packages_button")
@@ -56,60 +108,75 @@ def packages_callback():
     os.system("cd taro2 && npm install")
     dpg.set_value(value="Finished downloading npm packages...", item="packages_text")
 
+
 def battle_royale_callback():
     shutil.copyfile("templates/BattleRoyale.json", "taro2/src/game.json")
     game_callback()
+
 
 def blank_template_callback():
     shutil.copyfile("templates/BlankTemplate.json", "taro2/src/game.json")
     game_callback()
 
+
 def cell_eater_callback():
     shutil.copyfile("templates/CellEater.json", "taro2/src/game.json")
     game_callback()
+
 
 def deathmatch_callback():
     shutil.copyfile("templates/Deathmatch.json", "taro2/src/game.json")
     game_callback()
 
+
 def guided_tutorial_callback():
     shutil.copyfile("templates/GuidedTutorial.json", "taro2/src/game.json")
     game_callback()
+
 
 def hunt_and_gather_callback():
     shutil.copyfile("templates/HuntAndGather.json", "taro2/src/game.json")
     game_callback()
 
+
 def platformer_callback():
     shutil.copyfile("templates/Platformer.json", "taro2/src/game.json")
     game_callback()
+
 
 def racer_callback():
     shutil.copyfile("templates/Racer.json", "taro2/src/game.json")
     game_callback()
 
+
 def soccer_callback():
     shutil.copyfile("templates/Soccer.json", "taro2/src/game.json")
     game_callback()
+
 
 def team_elimination_callback():
     shutil.copyfile("templates/TeamElimination.json", "taro2/src/game.json")
     game_callback()
 
+
 def tower_defense_callback():
     shutil.copyfile("templates/TowerDefense.json", "taro2/src/game.json")
     game_callback()
 
+
 def zombie_tag_callback():
     shutil.copyfile("templates/ZombieTag.json", "taro2/src/game.json")
     game_callback()
+
 
 def game_callback():
     dpg.hide_item("setup_project")
     dpg.delete_item("setup_project_text")
     dpg.delete_item("setup_project_button")
     dpg.add_text("Update Project:", parent="default_window")
-    dpg.add_button(label="Update", parent="default_window", callback=setup_project_callback)
+    dpg.add_button(
+        label="Update", parent="default_window", callback=setup_project_callback
+    )
     dpg.add_text("Edit Game Settings:", parent="default_window")
     dpg.add_button(label="Edit", parent="default_window", callback=edit_callback)
     dpg.add_text("Play the game:", parent="default_window")
