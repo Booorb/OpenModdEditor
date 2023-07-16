276aad6b82153319dde53443f9b4541314a6a902
Borb <70481781+Booorb@users.noreply.github.com>
2023-07-16 11:22:49 +0200

    added change project functionality

--- src/index.py
+++ src/index.py
@@ -1,6 +1,8 @@
 import dearpygui.dearpygui as dpg
 import os
 import webbrowser
+import json
+import shutil
 from setup import setup_project_callback
 from setup import taro2_callback
 from setup import packages_callback
@@ -14,9 +16,50 @@ dpg.create_context()
 dpg.create_viewport()
 dpg.setup_dearpygui()
 
+
+
+def open_callback(sender, app_data):
+    print("Sender: ", sender)
+    print("App Data: ", app_data["file_path_name"])
+    with open("storage.json") as f:
+        data = json.load(f)
+        data["folder"] = app_data["file_path_name"]
+        json.dump(data, open("storage.json", "w"), indent=4)
+        shutil.copyfile("storage.json", data["folder"] + "/storage.json")
+        shutil.copyfile(
+            "OpenSans-Regular.ttf", data["folder"] + "/OpenSans-Regular.ttf"
+        )
+        shutil.copytree("templates", data["folder"] + "/templates", dirs_exist_ok=True)
+        os.chdir(data["folder"])
+
+
+dpg.add_file_dialog(
+    directory_selector=True,
+    show=False,
+    callback=open_callback,
+    tag="file_dialog_id",
+    width=700,
+    height=400,
+)
+
 with dpg.window(label="Menu", tag="default_window"):
+    with open("storage.json") as f:
+        data = json.load(f)
+        shutil.copyfile("storage.json", data["folder"] + "/storage.json")
+        shutil.copyfile(
+            "OpenSans-Regular.ttf", data["folder"] + "/OpenSans-Regular.ttf"
+        )
+        shutil.copytree("templates", data["folder"] + "/templates", dirs_exist_ok=True)
+        os.chdir(data["folder"])
     dpg.set_primary_window("default_window", True)
+    dpg.bind_font(default_font)
     if os.path.isfile("taro2/src/game.json"):
+        dpg.add_text("Change Project:")
+        dpg.add_button(
+            label="Change",
+            tag="setup_open_button",
+            callback=lambda: dpg.show_item("file_dialog_id"),
+        )
         dpg.add_text("Update Project:")
         dpg.add_button(label="Update", callback=setup_project_callback)
         dpg.add_text("Edit Game Settings:")
@@ -25,12 +68,18 @@ with dpg.window(label="Menu", tag="default_window"):
         dpg.add_button(label="Play", callback=play_callback)
     else:
         dpg.add_text("Create Project:", tag="setup_project_text")
-        dpg.add_button(label="Create", tag="setup_project_button", callback=setup_project_callback)
-        dpg.add_text("Open Project:")
-        dpg.add_button(label="Open", tag="setup_open_button")
-    
+        dpg.add_button(
+            label="Create", tag="setup_project_button", callback=setup_project_callback
+        )
+        dpg.add_text("Change Project:")
+        dpg.add_button(
+            label="Change",
+            tag="setup_open_button",
+            callback=lambda: dpg.show_item("file_dialog_id"),
+        )
 
-dpg.create_viewport(title='OpenGameBuilder', width=800, height=600)
+
+dpg.create_viewport(title="OpenGameBuilder", width=800, height=600)
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
