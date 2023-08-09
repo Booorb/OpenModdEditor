e63bc492192fb51bf031af5b1b5992299b68a9df
Borb <maximilian.riel@gmail.com>
2023-08-09 22:37:36 +0200

    show existing dialogues

--- src/world/dialogues.py
+++ src/world/dialogues.py
@@ -1,6 +1,13 @@
 import dearpygui.dearpygui as dpg
+import json
 
 
 def dialogues_callback():
     with dpg.window(label="Dialogoues", tag="dialogues_window"):
-        dpg.add_text("lorem ipsum")
+        with open("taro2/src/game.json") as f:
+            data = json.load(f)
+            dpg.add_button(label="New Dialogue")
+            dpg.add_separator()
+            if "dialogues" in data["data"].keys():
+                for dialogues in data["data"]["dialogues"]:
+                    dpg.add_button(label=dialogues)
