3df4f8f591a6045fc36444342d4493b75fbc12a4
Borb <maximilian.riel@gmail.com>
2023-08-15 19:44:52 +0200

    add compile scripts callback

--- src/scripts.py
+++ src/scripts.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 import json
 import webbrowser
+import os
 from re import sub
 
 
@@ -37,8 +38,22 @@ with open("settings.json") as f:
                     + "/entity_scripts.py"
                 )
 
+    def compile_scripts_callback():
+        with open(editorFolder + "/settings.json") as f:
+            data = json.load(f)
+            with open(data["gameFolder"] + "/taro2/src/game.json") as f:
+                game = json.load(f)
+                os.system(
+                    "cd "
+                    + data["gameFolder"]
+                    + "/"
+                    + snake_case(game["title"])
+                    + " && pymodd compile"
+                )
+
 
 def script_editor_callback():
     with dpg.window(label="Scripts", tag="scripts_window"):
         dpg.add_button(label="Global Scripts", callback=global_scripts_callback)
         dpg.add_button(label="Entity Scripts", callback=entity_scripts_callback)
+        dpg.add_button(label="Compile Scripts", callback=compile_scripts_callback)
