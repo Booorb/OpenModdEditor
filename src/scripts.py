b88782df5a452f107fb73db54de884a641b046f6
Borb <70481781+Booorb@users.noreply.github.com>
2023-08-20 15:39:26 +0200

    check if game.json exists

--- src/scripts.py
+++ src/scripts.py
@@ -16,47 +16,48 @@ def snake_case(s):
 
 with open("settings.json") as f:
     data = json.load(f)
-    with open(data["gameFolder"] + "/taro2/src/game.json") as f:
-        game = json.load(f)
+    if os.path.isfile(data["gameFolder"] + "taro2/src/game.json"):
+        with open(data["gameFolder"] + "/taro2/src/game.json") as f:
+            game = json.load(f)
 
-        def global_scripts_callback():
-            webbrowser.open(
-                data["gameFolder"] + "/" + snake_case(game["title"]) + "/scripts.py"
-            )
-
-        def entity_scripts_callback():
-            webbrowser.open(
-                data["gameFolder"]
-                + "/"
-                + snake_case(game["title"])
-                + "/entity_scripts.py"
-            )
-
-        def compile_scripts_callback():
-            os.system(
-                "cd "
-                + data["gameFolder"]
-                + "/"
-                + snake_case(game["title"])
-                + " && pymodd compile"
-            )
-            json_names = [
-                filename
-                for filename in os.listdir(
-                    data["gameFolder"] + "/" + snake_case(game["title"] + "/output")
+            def global_scripts_callback():
+                webbrowser.open(
+                    data["gameFolder"] + "/" + snake_case(game["title"]) + "/scripts.py"
                 )
-                if filename.endswith(".json")
-            ]
 
-            for json_name in json_names:
-                shutil.copyfile(
+            def entity_scripts_callback():
+                webbrowser.open(
                     data["gameFolder"]
                     + "/"
                     + snake_case(game["title"])
-                    + "/output/"
-                    + json_name,
-                    data["gameFolder"] + "/taro2/src/game.json",
+                    + "/entity_scripts.py"
                 )
+
+            def compile_scripts_callback():
+                os.system(
+                    "cd "
+                    + data["gameFolder"]
+                    + "/"
+                    + snake_case(game["title"])
+                    + " && pymodd compile"
+                )
+                json_names = [
+                    filename
+                    for filename in os.listdir(
+                        data["gameFolder"] + "/" + snake_case(game["title"] + "/output")
+                    )
+                    if filename.endswith(".json")
+                ]
+
+                for json_name in json_names:
+                    shutil.copyfile(
+                        data["gameFolder"]
+                        + "/"
+                        + snake_case(game["title"])
+                        + "/output/"
+                        + json_name,
+                        data["gameFolder"] + "/taro2/src/game.json",
+                    )
 
 
 def script_editor_callback():
