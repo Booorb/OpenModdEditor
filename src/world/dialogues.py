15392abf5a11db5012bbf11159ece3d9fb9faec5
Borb <maximilian.riel@gmail.com>
2023-08-22 19:25:05 +0200

    add new option callback

--- src/world/dialogues.py
+++ src/world/dialogues.py
@@ -2,6 +2,39 @@ import dearpygui.dearpygui as dpg
 import json
 
 
+def new_option_callback():
+    if dpg.does_item_exist("new_option_window"):
+        dpg.show_item("new_option_window")
+    else:
+        with dpg.window(label="New Option", tag="new_option_window"):
+            with open(gameFolder + "/taro2/src/game.json") as f:
+                data = json.load(f)
+                dpg.add_text("Name:")
+                dpg.add_input_text(tag="new_option_name")
+                dpg.add_text("Run script:")
+                script_list = []
+                if "scripts" in data["data"].keys():
+                    for script in data["data"]["scripts"]:
+                        script_list.append(script)
+                    dpg.add_combo(
+                        items=script_list,
+                        tag="select_script_button",
+                    )
+                else:
+                    dpg.add_text("no scripts exist, please create a script first!")
+                dpg.add_text("Follow-Up Dialogue:")
+                dialogue_list = []
+                if "dialogues" in data["data"].keys():
+                    for dialogue in data["data"]["dialogues"]:
+                        dialogue_list.append(dialogue)
+                    dpg.add_combo(
+                        items=dialogue_list,
+                        tag="select_follow_up_dialogue_button",
+                    )
+                else:
+                    dpg.add_text("no dialogues exist, please create a dialogue first!")
+
+
 def new_dialogue_callback():
     if dpg.does_item_exist("update_dialogue_window"):
         dpg.delete_item("update_dialogue_window")
@@ -21,6 +54,8 @@ def new_dialogue_callback():
             dpg.add_input_text(tag="new_dialogue_image")
             dpg.add_text("Letter Print Speed:")
             dpg.add_slider_int(tag="new_dialogue_print_speed")
+            dpg.add_text("Options:")
+            dpg.add_button(label="New Option", callback=new_option_callback)
             dpg.add_button(label="Save", callback=save_callback)
 
 
@@ -109,6 +144,15 @@ def save_callback():
             "message": dpg.get_value("new_dialogue_message"),
             "image": dpg.get_value("new_dialogue_image"),
             "letterPrintSpeed": dpg.get_value("new_dialogue_print_speed"),
+            "options": [
+                {
+                    "name": dpg.get_value("new_option_name"),
+                    "scriptName": dpg.get_value("select_script_button"),
+                    "followUpDialogue": dpg.get_value(
+                        "select_follow_up_dialogue_button"
+                    ),
+                }
+            ],
         }
         json.dump(data, open(gameFolder + "/taro2/src/game.json", "w"), indent=4)
         if dpg.does_item_exist("new_dialogue_window"):
