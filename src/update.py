5a20cf39018c2f30852de33ca325a4b2bf6fd9f1
Borb <70481781+Booorb@users.noreply.github.com>
2023-08-27 14:00:42 +0200

    update update manager

--- src/update.py
+++ src/update.py
@@ -1,24 +1,27 @@
 import dearpygui.dearpygui as dpg
 import os
+import json
 
 
-def update_taro2_callback():
-    os.system("cd taro2 && git pull")
-
-
-def update_packages_callback():
-    os.system("cd taro2 && npm install")
+def start_update_callback():
+    os.system("cd taro2 && git pull && npm install")
+    with open("settings.json") as f:
+        settings = json.load(f)
+        os.system("cd " + settings["editorFolder"] + "  && git pull")
 
 
 def update_project_callback():
-    if dpg.does_item_exist("update_project"):
-        dpg.show_item("update_project")
+    if dpg.does_item_exist("updating_popup"):
+        dpg.configure_item("updating_popup", show=True)
     else:
-        with dpg.window(label="Update Manager", tag="update_project"):
-            dpg.add_text("Update Taro2:")
-            dpg.add_button(label="Update", callback=update_taro2_callback)
-            dpg.add_text("Update npm packages:")
-            dpg.add_button(
-                label="Update",
-                callback=update_packages_callback,
-            )
+        with dpg.window(
+            modal=True,
+            show=True,
+            tag="updating_popup",
+            no_title_bar=True,
+        ):
+            dpg.add_text("Updating OpenModdEditor...")
+            dpg.add_text("Please wait!")
+    start_update_callback()
+    if dpg.does_item_exist("updating_popup"):
+        dpg.configure_item("updating_popup", show=False)
